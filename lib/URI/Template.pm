2ea016628088df906dda94a2a0d904c124be8f94
Brian Cassidy <bricas@cpan.org>
2008-02-12 18:24:11 +0000

    Import of BRICAS/URI-Template-0.13 from CPAN.
    gitpan-cpan-distribution: URI-Template
    gitpan-cpan-version:      0.13
    gitpan-cpan-path:         BRICAS/URI-Template-0.13.tar.gz
    gitpan-cpan-author:       BRICAS
    gitpan-cpan-maturity:     released

--- lib/URI/Template.pm
+++ lib/URI/Template.pm
@@ -3,7 +3,7 @@ package URI::Template;
 use strict;
 use warnings;
 
-our $VERSION = '0.12';
+our $VERSION = '0.13';
 
 use URI;
 use URI::Escape ();
@@ -172,9 +172,7 @@ sub deparse {
         my $templ = $self->as_string;
         $self->{ vars_list } = [ $templ =~ /{(.+?)}/g ];
         $templ =~ s/{.+?}/(.+?)/g;
-        # If the template ends w/ a match, then make it greedy.
-        $templ =~ s/\Q(.+?)\E$/(.+)/;
-        $self->{ deparse_re } = qr/$templ/;
+        $self->{ deparse_re } = qr/^${templ}$/;
     }
 
     my @matches = $uri =~ $self->{ deparse_re };
